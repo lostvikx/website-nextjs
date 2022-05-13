eccfc37a7826429076f967e1f32e33b5e51867a1
Vikram Negi <viknegi0@gmail.com>
2022-05-13 23:16:10 +0530

    woah

--- pages/about.js
+++ pages/about.js
@@ -6,23 +6,23 @@ export default function About() {
       
       <h1>The Entire History of Me</h1>
 
-      <p className="article-p">
+      <p>
         I&apos;m a self-taught developer who enjoys using his computer for doing productive things. I was always passionate about computers since the tender age of ten. It all started when my parents bought me a brand new desktop computer.
       </p>
 
-      <p className="article-p">
+      <p>
         I didn&apos;t know what the internet was back then, so I used to mess around on my computer by creating doodles on MS Paint and playing those retro in-build Windows XP games.
       </p>
 
       <h2>Coding Journey</h2>
 
-      <p className="article-p">
+      <p>
         Learning to code on your own comes with its own sets of challenges. Such as not knowing how to start learning or what to learn in the first place. It&apos;s the persistent effort and determination that allowed me to look at the other side of the wall.
       </p>
-      <p className="article-p">
+      <p>
         I started programming because I wanted my dream, to create my own company someday, to come true. While pursuing my vision, I found a new hobby. I quickly developed a passion for programming, which has evolved into something I love doing.
       </p>
-      <p className="article-p">
+      <p>
         The first-ever programming language that I learned was JavaScript. Even though it is not a perfect language, it has a few core design errors, but nothing in life is perfect.
       </p>
 
--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,17 @@ export default function Home() {
       
       <section className="showcase">
         <div className="show-img">
-          <img
-          src={"/images/showcase.jpg"}
-          alt="Me"
+          <Image
+          src={"/images/showcase.jpg" || "/images/default-fallback-image.png"}
+          alt="Me chilling near a delta"
           width="300px"
-          height="600px" 
+          height="600px"
+          layout="fixed" 
         />
         </div>
         <div className="show-text flex-col">
           <h1>Hello, World 👋</h1>
-          <p>I&apos;m a self-taught developer who enjoys using his computer for doing productive things.</p>
+          <p>I&apos;m a self-taught developer who enjoys using his computer to do productive things in life.</p>
           <div className="fn-btns flex-row">
             <a className="btn" href="mailto:viknegi0@gmail.com">Contact</a>
             <a className="btn" href="/resume_vikram_negi.pdf" download>Download Resume</a>
